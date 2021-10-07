6bd21a48e2318e799ca0852fcf031feaac50290d
jung-han <hanjung@linecorp.com>
2021-10-07 19:44:52 +0900

    (chap12) 12.2.2 문제 수정(css 위치 수정, 클래스 명 변경)

--- chapter12-debug/js/index.js
+++ chapter12-debug/js/index.js
@@ -2,8 +2,8 @@ import { response } from "./response";
 import { makeFrameworkModel, renderCard, bindCardEvent } from "./card";
 import { showModal } from "./modal";
 
-import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css';
 
 const frontEndFrameworkCards = makeFrameworkModel(response);
 
